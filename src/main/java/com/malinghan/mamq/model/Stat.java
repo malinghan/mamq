9d89075c56ce55dadbc73e61e4d0f8d576c1f367
mattma <linghan.ma@gmail.com>
2026-02-28 23:39:06 +0800

    fix

--- src/main/java/com/malinghan/mamq/model/Stat.java
+++ src/main/java/com/malinghan/mamq/model/Stat.java
@@ -2,9 +2,11 @@ package com.malinghan.mamq.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Stat {
     private int total;
     private int position;
